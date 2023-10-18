2fa90305eb69186bae01f18e5963b3f2823ee5c1
Jiayi-Wang-Joey <591813079@qq.com>
2023-10-18 12:26:54 +0200

    add technical data

--- code/utils.R
+++ code/utils.R
@@ -5,7 +5,8 @@ addAnnotations <- \(motif_ix) {
     mmMat <- assays(motif_ix)$motifMatches
     as(as(as(as.matrix(mmMat), "dMatrix"), "generalMatrix"), "TsparseMatrix")
     #mmMat <- as(as.matrix(mmMat), "dgTMatrix")
-    mmMat <- as(as(as(as.matrix(mmMat), "dMatrix"), "generalMatrix"), "TsparseMatrix")
+    mmMat <- as(as(as(as.matrix(mmMat), "dMatrix"), 
+        "generalMatrix"), "TsparseMatrix")
     mmDt <- data.table(i = mmMat@i,j = mmMat@j,x = mmMat@x)
     ann <- as.data.table(ann)
     ann[,peak_id := 1:nrow(ann)]
@@ -57,11 +58,13 @@ getNonRedundantMotifs <- function(format=c("PFMatrix","universal","PWMatrix"),
     species=c("Hsapiens","Mmusculus")){
     species <- match.arg(species)
     motifs <- MotifDb::query(MotifDb::MotifDb, c(species,"HOCOMOCO"))
-    pat <- paste0("^",species,"-HOCOMOCOv1[0-1]-|_HUMAN.+|_MOUSE.+|core-[A-D]-|secondary-[A-D]-")
+    pat <- paste0("^",species,
+        "-HOCOMOCOv1[0-1]-|_HUMAN.+|_MOUSE.+|core-[A-D]-|secondary-[A-D]-")
     modf <- data.frame(row.names=names(motifs),
         TF=gsub(pat,"",names(motifs)),
         grade=gsub(".+\\.","",names(motifs)))
-    modf <- modf[order(modf$TF,-as.numeric(grepl("HOCOMOCOv11",row.names(modf))),modf$grade),]
+    modf <- modf[order(modf$TF,-as.numeric(grepl("HOCOMOCOv11",
+        row.names(modf))),modf$grade),]
     modf <- modf[!duplicated(modf$TF),]
     motifs <- motifs[row.names(modf)]
     switch(match.arg(format),
@@ -73,4 +76,59 @@ getNonRedundantMotifs <- function(format=c("PFMatrix","universal","PWMatrix"),
             setNames(universalmotif::convert_motifs(motifs, 
                 class="TFBSTools-PWMatrix"), modf$TF))
     )
+}
+
+
+FQnorm <- function(counts, type="mean"){
+    rk <- apply(counts,2,rank,ties.method='min')
+    counts.sort <- apply(counts,2,sort)
+    if(type=="mean"){
+        # refdist <- apply(counts.sort,1,mean)
+        refdist <- base::rowMeans(counts.sort)
+    } else if(type=="median"){
+        #refdist <- apply(counts.sort,1,median)
+        refdist <- matrixStats::rowMedians(counts.sort)
+    }
+    norm <- apply(rk,2,function(r){ refdist[r] })
+    rownames(norm) <- rownames(counts)
+    return(norm)
+}
+
+gcqn <- function(counts, gcGroups, summary='mean', round=TRUE){
+    gcBinNormCounts <- matrix(NA, nrow=nrow(counts), ncol=ncol(counts), 
+        dimnames=list(rownames(counts),colnames(counts)))
+    for(ii in 1:nlevels(gcGroups)){
+        id <- which(gcGroups==levels(gcGroups)[ii])
+        if(length(id) == 1){
+            normCountBin <- counts[id,]
+            if(round) normCountBin <- round(normCountBin)
+            gcBinNormCounts[id,] <- normCountBin
+            next
+        }
+        countBin <- counts[id,,drop=FALSE]
+        if(summary=="mean"){
+            normCountBin <- FQnorm(countBin, type='mean')
+        } else if(summary=="median"){
+            normCountBin <- FQnorm(countBin, type='median')
+        }
+        if(round) normCountBin <- round(normCountBin)
+        normCountBin[normCountBin<0] <- 0
+        gcBinNormCounts[id,] <- normCountBin
+    }
+    return(gcBinNormCounts)
+}
+
+gcqn_qsmooth <- function(counts, gcGroups, bio){
+    gcBinNormCounts <- matrix(NA, nrow=nrow(counts), ncol=ncol(counts), 
+        dimnames=list(rownames(counts),colnames(counts)))
+    for(ii in 1:nlevels(gcGroups)){
+        id <- which(gcGroups==levels(gcGroups)[ii])
+        countBin <- counts[id,]
+        qs <- qsmooth(countBin, group_factor=bio)
+        normCountBin <- qs@qsmoothData
+        normCountBin <- round(normCountBin)
+        normCountBin[normCountBin<0] <- 0
+        gcBinNormCounts[id,] <- normCountBin
+    }
+    return(gcBinNormCounts)
 }
